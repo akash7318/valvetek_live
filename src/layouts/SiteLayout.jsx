0f78fcfb980b054f9a1145f71c22bff65f50af0b
Akash Sharma <aks.85.sky@gmail.com>
2024-03-11 14:12:03 +0530

    product routes

--- src/layouts/SiteLayout.jsx
+++ src/layouts/SiteLayout.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import { Routes, Route } from 'react-router-dom';
 import '../App.css';
 import Nav from '../components/nav/Nav';
@@ -7,12 +7,36 @@ import About from '../About';
 import Footer from '../components/sections/Footer';
 
 function SiteLayout() {
+
+    const [products, setProduct] = useState([]);
+
+    useEffect(() => {
+        getProducts();
+    }, [])
+
+    const getProducts = async () => {
+        let result = await fetch(`${process.env.REACT_APP_BASE_URL}products`);
+        result = await result.json();
+        if (result.status) {
+            setProduct(result.products);
+        }
+    }
+
     return (
         <>
             <Nav></Nav>
             <Routes>
                 <Route path='/' element={<Home />} />
                 <Route path='/about' element={<About />} />
+                {
+                    products
+                        ?
+                        products.map((value, index) =>
+                            <Route key={index} path={'/' + value.slug} element={<About />} />
+                        )
+                        :
+                        null
+                }
             </Routes>
             <Footer></Footer>
         </>
